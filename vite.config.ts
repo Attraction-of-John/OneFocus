d3740414b3dd62ca701527d5988fbcdfa8cc7783
Kim Do Young <difbfl4750@gmail.com>
2024-12-28 20:51:27 +0900

    chore: tailwind & vite & swc & storybook & cypress & vitest setting, yarn migration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {},
+    baseUrl: 'http://localhost:5173',
+    supportFile: false
+  },
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'vite'
+    }
+  }
   plugins: [react()],
 })
